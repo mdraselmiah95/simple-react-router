857158192702af630ee77e74432aba9c05b6dd46
rasel <russelroy15@gmail.com>
2021-09-29 20:01:49 +0600

    WIP on react router

--- src/App.js
+++ src/App.js
@@ -1,5 +1,7 @@
 import { BrowserRouter, Route, Switch } from "react-router-dom";
 import "./App.css";
+import About from "./components/About/About";
+import Friends from "./components/Friends/Friends";
 import Home from "./components/Home/Home";
 
 function App() {
@@ -10,6 +12,12 @@ function App() {
         <Switch>
           <Route path="/home">
             <Home />
+          </Route>
+          <Route path="/about">
+            <About />
+          </Route>
+          <Route path="/friends">
+            <Friends />
           </Route>
         </Switch>
       </BrowserRouter>
