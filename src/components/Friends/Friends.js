21b89295630534d1dcecfc055200b5b9ca698b5c
rasel <russelroy15@gmail.com>
2021-09-30 01:44:07 +0600

    WIP on react router

--- src/components/Friends/Friends.js
+++ src/components/Friends/Friends.js
@@ -1,9 +1,16 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 
 const Friends = () => {
+  const [users, setUsers] = useState([]);
+
+  useEffect(() => {
+    fetch("https://jsonplaceholder.typicode.com/users")
+      .then((res) => res.json())
+      .then((data) => setUsers(data));
+  }, []);
   return (
     <div>
-      <h2>This is friends Page.</h2>
+      <h2>This is friends Page.{users.length}</h2>
     </div>
   );
 };
