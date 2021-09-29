7450c5662a8dc3c2bcd4fe3d261d7aab495adb25
rasel <russelroy15@gmail.com>
2021-09-29 19:57:57 +0600

    WIP on react router

--- src/App.js
+++ src/App.js
@@ -1,9 +1,13 @@
+import { BrowserRouter, Switch } from "react-router-dom";
 import "./App.css";
 
 function App() {
   return (
     <div className="App">
       <h2>React router</h2>
+      <BrowserRouter>
+        <Switch></Switch>
+      </BrowserRouter>
     </div>
   );
 }
