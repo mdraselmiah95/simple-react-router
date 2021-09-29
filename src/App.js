a8ea288c7fb14c7451e0e89ab791fd31ca45b02d
rasel <russelroy15@gmail.com>
2021-09-29 19:56:38 +0600

    WIP on react router

--- src/App.js
+++ src/App.js
@@ -1,16 +1,9 @@
 import "./App.css";
-import About from "./components/About/About";
-import Friends from "./components/Friends/Friends";
-import Home from "./components/Home/Home";
-import NotFound from "./components/NotFound/NotFound";
 
 function App() {
   return (
     <div className="App">
-      <Home />
-      <Friends />
-      <About />
-      <NotFound />
+      <h2>React router</h2>
     </div>
   );
 }
