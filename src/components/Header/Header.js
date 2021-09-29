89b46cca10618a2441feb21fb90fb323d67a80e8
rasel <russelroy15@gmail.com>
2021-09-30 02:54:06 +0600

    WIP on react router

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,12 +1,22 @@
 import React from "react";
-import { Link } from "react-router-dom";
+import { Link as NavLink } from "react-router-dom";
 import "./Header.css";
 const Header = () => {
+  const activeStyle = {
+    fontWeight: "bold",
+    color: "red",
+  };
   return (
-    <nav className="header">
-      <Link to="/home">Home</Link>
-      <Link to="/friends">Friends</Link>
-      <Link to="/about">About</Link>
+    <nav>
+      <NavLink activeStyle={activeStyle} to="/home">
+        Home
+      </NavLink>
+      <NavLink activeStyle={activeStyle} to="/friends">
+        Friends
+      </NavLink>
+      <NavLink activeStyle={activeStyle} to="/about">
+        About
+      </NavLink>
     </nav>
   );
 };
