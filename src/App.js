a0d5363ac34f1954d5a8d8088d105b677c271db6
rasel <russelroy15@gmail.com>
2021-09-29 20:02:21 +0600

    WIP on react router

--- src/App.js
+++ src/App.js
@@ -7,7 +7,6 @@ import Home from "./components/Home/Home";
 function App() {
   return (
     <div className="App">
-      <h2>React router</h2>
       <BrowserRouter>
         <Switch>
           <Route path="/home">
