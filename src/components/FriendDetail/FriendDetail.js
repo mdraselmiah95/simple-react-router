823b51190ff2581fa4ddc3c20a6222a73b39171d
rasel <russelroy15@gmail.com>
2021-09-30 03:39:45 +0600

    WIP on react router

--- src/components/FriendDetail/FriendDetail.js
+++ src/components/FriendDetail/FriendDetail.js
@@ -1,8 +1,15 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { useParams } from "react-router";
 
 const FriendDetail = () => {
   const { friendId } = useParams();
+  const [friend, setFriend] = useState({});
+  useEffect(() => {
+    const url = `https://jsonplaceholder.typicode.com/users/${friendId}`;
+    fetch(url)
+      .then((res) => res.json())
+      .then((data) => setFriend(data));
+  }, []);
   return (
     <div>
       <h3>This is friend details: {friendId}</h3>
