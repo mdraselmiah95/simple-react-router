410a6343e4e03dacb6a55fe05b6328d47de81444
rasel <russelroy15@gmail.com>
2021-09-30 02:43:00 +0600

    WIP on react router

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import "./App.css";
 import About from "./components/About/About";
 import Friends from "./components/Friends/Friends";
+import Header from "./components/Header/Header";
 import Home from "./components/Home/Home";
 import NotFound from "./components/NotFound/NotFound";
 
@@ -9,6 +10,7 @@ function App() {
   return (
     <div className="App">
       <Router>
+        <Header />
         <Switch>
           <Route exact path="/">
             <Home />
