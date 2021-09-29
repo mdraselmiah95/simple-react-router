15a8abf083cf2ae2e49d1763e32f0c6e2465ff62
rasel <russelroy15@gmail.com>
2021-09-29 20:04:49 +0600

    WIP on react router

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import "./App.css";
 import About from "./components/About/About";
 import Friends from "./components/Friends/Friends";
 import Home from "./components/Home/Home";
+import NotFound from "./components/NotFound/NotFound";
 
 function App() {
   return (
@@ -17,6 +18,9 @@ function App() {
           </Route>
           <Route path="/friends">
             <Friends />
+          </Route>
+          <Route path="*">
+            <NotFound />
           </Route>
         </Switch>
       </BrowserRouter>
