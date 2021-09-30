098693b6057779caa2c9100f4aadd8d5d54cd6e3
rasel <russelroy15@gmail.com>
2021-09-30 15:05:10 +0600

    WIP on react router

--- src/components/Post/Post.js
+++ src/components/Post/Post.js
@@ -2,11 +2,10 @@ import React from "react";
 import { Link } from "react-router-dom";
 
 const Post = (props) => {
-  const { title, body, id } = props.post;
+  const { title, id } = props.post;
   return (
     <div>
       <h2>{title}</h2>
-      <p>{body}</p>
       <Link to={`/post/${id}`}>post details</Link>
     </div>
   );
--- src/components/PostDetail/PostDetail.js
+++ src/components/PostDetail/PostDetail.js
@@ -1,9 +1,20 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import { useParams } from "react-router";
 
 const PostDetail = () => {
+  const { postId } = useParams();
+  const [post, setPost] = useState({});
+  useEffect(() => {
+    const url = `https://jsonplaceholder.typicode.com/posts/${postId}`;
+    fetch(url)
+      .then((res) => res.json())
+      .then((data) => setPost(data));
+  }, []);
   return (
     <div>
-      <h2>post details</h2>
+      <h2>post details: {postId}</h2>
+      <h5>{post.title}</h5>
+      <p>{post.body}</p>
     </div>
   );
 };
