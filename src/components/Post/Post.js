69718760cf8636b363fc6c05d5883bb0e22512f2
rasel <russelroy15@gmail.com>
2021-09-30 15:08:36 +0600

    WIP on react router

--- src/components/Post/Post.js
+++ src/components/Post/Post.js
@@ -1,12 +1,19 @@
 import React from "react";
-import { Link } from "react-router-dom";
+import { Link, useHistory } from "react-router-dom";
 
 const Post = (props) => {
   const { title, id } = props.post;
+  const history = useHistory();
+
+  const handleClick = () => {
+    history.push(`/post/${id}`);
+  };
   return (
     <div>
       <h2>{title}</h2>
       <Link to={`/post/${id}`}>post details</Link>
+      <br />
+      <button onClick={handleClick}>Click to see details</button>
     </div>
   );
 };
