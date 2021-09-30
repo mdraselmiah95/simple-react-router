1da9bab79916950fb6f4be529ec0c5e6194ff164
rasel <russelroy15@gmail.com>
2021-09-30 14:41:17 +0600

    WIP on react router

--- src/components/FriendDetail/FriendDetail.js
+++ src/components/FriendDetail/FriendDetail.js
@@ -1,21 +1,27 @@
 import React, { useEffect, useState } from "react";
-import { useParams } from "react-router";
+import { useHistory, useParams } from "react-router";
 
 const FriendDetail = () => {
   const { friendId } = useParams();
   const [friend, setFriend] = useState({});
+  const history = useHistory();
   useEffect(() => {
     const url = `https://jsonplaceholder.typicode.com/users/${friendId}`;
     fetch(url)
       .then((res) => res.json())
       .then((data) => setFriend(data));
   }, []);
+
+  const handleClick = () => {
+    history.push("/friends");
+  };
   return (
     <div>
       <h3>This is friend details: {friendId}</h3>
       <h1>{friend.name}</h1>
       <h3>Phone: {friend.phone}</h3>
       <p>Works at: {friend.company?.name}</p>
+      <button onClick={handleClick}>See All Friends</button>
     </div>
   );
 };
